78235942337f09aed303993da5de97d3a5ba5c25
seraytugcu <seraytugcu@outlook.com>
2022-08-26 17:34:58 -0400

    mortgage 2022

---
+++ src/Calculate.java
@@ -0,0 +1,8 @@
+public class Calculate {
+
+    public static void main(String[] args) {
+        Calculator mortgage = new Calculator();
+        String mortgagePayment = mortgage.Mortgage();
+        System.out.println(mortgagePayment);
+    }
+}
